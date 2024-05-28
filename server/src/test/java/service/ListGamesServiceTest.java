a98f954b97cae082b0f1485e13a68b6b29496ebc
Samuel Galbraith <samuelgalbraith.school@gmail.com>
2024-05-28 09:20:00 -0600

    Added listGames Tests

---
+++ server/src/test/java/service/ListGamesServiceTest.java
@@ -0,0 +1,53 @@
+package service;
+
+import chess.ChessGame;
+import dataaccess.DataAccessException;
+import dataaccess.dao.AuthDAO;
+import dataaccess.dao.GameDAO;
+import dataaccess.dao.internalDAO.AuthInternalDAO;
+import dataaccess.dao.internalDAO.GameInternalDAO;
+import model.AuthData;
+import model.GameData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ListGamesServiceTest {
+
+    private AuthDAO auths;
+    private GameDAO games;
+    private ListGamesService gameLister;
+
+
+    @BeforeEach
+    void setup() {
+        auths = new AuthInternalDAO();
+        games = new GameInternalDAO();
+    }
+
+    @Test
+    void testListGames() throws DataAccessException {
+        String username = "SirChessGamer";
+        String authToken = "validToken";
+        String gameID = "game123";
+        ChessGame newgame = new ChessGame();
+
+        auths.createAuth(new AuthData(authToken, username));
+        GameData gameTest = new GameData(gameID, null, null, "The one true Game", newgame);
+        games.createGame(gameTest);
+        gameLister = new ListGamesService();
+
+        gameLister.listGamesService(games, auths, authToken);
+
+    }
+
+    @Test
+    void testInvalidAuthtoken() throws DataAccessException {
+        String authToken = "NotAValidTokken";
+        gameLister = new ListGamesService();
+        assertThrows(DataAccessException.class, () -> {
+            gameLister.listGamesService(games, auths, authToken);
+        });
+    }
+}
