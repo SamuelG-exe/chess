9e74678bf41f8c2e528930831179fd77619e6039
Samuel Galbraith <samuelgalbraith.school@gmail.com>
2024-06-07 15:25:53 -0600

    port 0

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -27,7 +27,7 @@ public class ServerFacadeTests {
     @BeforeAll
     public static void init()  {
         server = new Server();
-        var port = server.run(8080);
+        var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
         facade = new ServerFacade(Integer.toString(port));
     }
