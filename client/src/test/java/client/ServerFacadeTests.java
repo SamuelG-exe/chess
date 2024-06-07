45c3b824af96d3db6e15e04a01ff7485515515e2
Samuel Galbraith <samuelgalbraith.school@gmail.com>
2024-06-07 15:24:19 -0600

    Wrote tests but has error

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -4,7 +4,13 @@ import chess.ChessGame;
 import dataaccess.DataAccessException;
 import model.GameData;
 import org.junit.jupiter.api.*;
+import request.CreateGameReq;
+import request.JoinGameReq;
+import request.LoginReq;
 import request.RegisterReq;
+import response.CreateGameResp;
+import response.ListGamesResp;
+import response.LoginResp;
 import response.RegisterResp;
 import server.Server;
 import ui.ServerFacade;
@@ -21,8 +27,9 @@ public class ServerFacadeTests {
     @BeforeAll
     public static void init()  {
         server = new Server();
-        var port = server.run(0);
+        var port = server.run(8080);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(Integer.toString(port));
     }
 
     @BeforeEach
@@ -37,14 +44,14 @@ public class ServerFacadeTests {
 
 
     @Test
-    void registerPos() throws Exception {
+    void testRegisterPos() throws Exception {
         RegisterReq registerReq = new RegisterReq("username", "password", "email");
         RegisterResp registerResp = facade.register(registerReq);
         assertNotNull(registerResp);
         assertEquals(registerReq.username(), registerResp.username());
     }
     @Test
-    void registerNeg() throws Exception {
+    void testRegisterNeg() throws Exception {
         RegisterReq registerReq = new RegisterReq(null, "password", "email");
         assertThrows(Exception.class, () -> {
             RegisterResp registerResp = facade.register(registerReq);
@@ -52,17 +59,146 @@ public class ServerFacadeTests {
     }
 
     @Test
-    void LoginPos() throws Exception {
+    void testLoginPos() throws Exception {
         RegisterReq registerReq = new RegisterReq("username", "password", "email");
-        RegisterResp registerResp = facade.register(registerReq);
-        assertNotNull(registerResp);
-        assertEquals(registerReq.username(), registerResp.username());
+        facade.register(registerReq);
+
+        LoginReq login = new LoginReq("username", "password");
+        LoginResp loggedIn = facade.login(login);
+
+        assertNotNull(loggedIn);
+        assertEquals(loggedIn.username(), login.username());
+    }
+
+    @Test
+    void testLoginNeg() throws Exception {
+        RegisterReq registerReq = new RegisterReq("username", "password", "email");
+        facade.register(registerReq);
+
+        LoginReq login = new LoginReq("username", "WrongPassword");
+        assertThrows(Exception.class, () -> {
+            LoginResp loggedIn = facade.login(login);
+        });
+    }
+
+    @Test
+    void testCreateGamePos() throws Exception {
+        RegisterReq registerReq = new RegisterReq("username", "password", "email");
+        RegisterResp resp = facade.register(registerReq);
+        String token = resp.authToken();
+        LoginReq login = new LoginReq("username", "password");
+        facade.login(login);
+
+        CreateGameReq makegame = new CreateGameReq("newGame");
+        CreateGameResp newGame = facade.createGame(makegame, token);
+        ListGamesResp listGamesResp = facade.listGames(token);
+
+        assertEquals(newGame.gameID(), listGamesResp.games().getFirst().gameID());
+    }
+
+    @Test
+    void testCreateGameNeg() throws Exception {
+        RegisterReq registerReq = new RegisterReq("username", "password", "email");
+        RegisterResp resp = facade.register(registerReq);
+        String token = resp.authToken();
+        LoginReq login = new LoginReq("username", "password");
+        facade.login(login);
+
+        CreateGameReq makegame = new CreateGameReq("newGame");
+        CreateGameResp newGame = facade.createGame(makegame, token);
+        assertThrows(Exception.class, () -> {
+            CreateGameResp newGameAgain = facade.createGame(makegame, token);
+        });
+    }
+
+    @Test
+    void testListGamesPos() throws Exception {
+        RegisterReq registerReq = new RegisterReq("username", "password", "email");
+        RegisterResp resp = facade.register(registerReq);
+        String token = resp.authToken();
+        LoginReq login = new LoginReq("username", "password");
+        facade.login(login);
+
+        CreateGameReq makegame = new CreateGameReq("newGame");
+        facade.createGame(makegame, token);
+
+        ListGamesResp listGamesResp = facade.listGames(token);
+
+        assertEquals(listGamesResp.games().size(), 1);
+    }
+
+    @Test
+    void testListGamesNeg() throws Exception {
+        RegisterReq registerReq = new RegisterReq("username", "password", "email");
+        RegisterResp resp = facade.register(registerReq);
+        String token = resp.authToken();
+        String wrongToken = "lolNope";
+        LoginReq login = new LoginReq("username", "password");
+        facade.login(login);
+
+        CreateGameReq makegame = new CreateGameReq("newGame");
+        facade.createGame(makegame, token);
+
+        assertThrows(Exception.class, () -> {
+            facade.listGames(wrongToken);
+        });
+    }
+
+    @Test
+    void testJoinGamePos() throws Exception {
+        RegisterReq registerReq = new RegisterReq("username", "password", "email");
+        RegisterResp resp = facade.register(registerReq);
+        String token = resp.authToken();
+        LoginReq login = new LoginReq("username", "password");
+        facade.login(login);
+
+        CreateGameReq makegame = new CreateGameReq("newGame");
+        CreateGameResp newGame = facade.createGame(makegame, token);
+        ListGamesResp listGamesResp = facade.listGames(token);
+
+
+        JoinGameReq join = new JoinGameReq("WHITE", newGame.gameID());
+
+        assertEquals("username", listGamesResp.games().getFirst().whiteUsername());
+    }
+
+    @Test
+    void testJoinGameNeg() throws Exception {
+        RegisterReq registerReq = new RegisterReq("username", "password", "email");
+        RegisterResp resp = facade.register(registerReq);
+        String token = resp.authToken();
+        LoginReq login = new LoginReq("username", "password");
+        facade.login(login);
+
+        CreateGameReq makegame = new CreateGameReq("newGame");
+        CreateGameResp newGame = facade.createGame(makegame, token);
+        ListGamesResp listGamesResp = facade.listGames(token);
+
+        assertThrows(Exception.class, () -> {
+            JoinGameReq join = new JoinGameReq("TACO", newGame.gameID());
+
+        });
+
     }
 
 
-    @Test
-    void clearTest() throws Exception {
 
+    @Test
+    void testClear() throws Exception {
+        RegisterReq registerReq = new RegisterReq("username", "password", "email");
+        RegisterResp resp = facade.register(registerReq);
+        String token = resp.authToken();
+        LoginReq login = new LoginReq("username", "password");
+        facade.login(login);
+
+        CreateGameReq makegame = new CreateGameReq("newGame");
+        facade.createGame(makegame, token);
+
+        ListGamesResp listGamesResp = facade.listGames(token);
+
+        assertEquals(listGamesResp.games().size(), 1);
+        facade.ClearDB();
+        assertEquals(listGamesResp.games().size(), 0);
 
     }
 }
